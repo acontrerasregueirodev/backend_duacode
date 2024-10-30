ffbae5c3fbf35aa1fb201c1d8e38cf9a075addc2
acontrerasregueirodev <adrian.contreras.rookie@gmail.com>
2024-10-30 12:31:46 +0100

    usuario loguea y desloguea

--- proyecto_duacode/login/views.py
+++ proyecto_duacode/login/views.py
@@ -13,15 +13,17 @@ def login_view(request):
         
         if user is not None:
             login(request, user)
-            welcome_message = f"Bienvenido, {user.username} {user.id}!"  # Mensaje de bienvenida
+            print(f"Bienvenido, {user.username} {user.id}!") #) Mensaje de bienvenida
             return JsonResponse({'message': user.id})
         
         else:
+            print({'message': "Credenciales incorrectas. Inténtalo de nuevo."})
             return JsonResponse({'message': "Credenciales incorrectas. Inténtalo de nuevo."}, status=400)
 
     return JsonResponse({'message': "Método no permitido."}, status=405)
 
 def logout_view(request):
+    print("intentando hacer logout")
     if request.user.is_authenticated:
         # Si el usuario está autenticado, realizar el logout
         username = request.user.username
@@ -29,8 +31,11 @@ def logout_view(request):
         
         # Comprobar si el usuario fue desconectado correctamente
         if not request.user.is_authenticated:
+            print(f"{username} has been logged out.")
             return JsonResponse({'message': f"{username} has been logged out."})
         else:
+            print('message Logout failed.')
             return JsonResponse({'message': "Logout failed."}, status=500)
     else:
+        print('message No user is logged in.')
         return JsonResponse({'message': "No user is logged in."}, status=400)
